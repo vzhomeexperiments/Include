ab93b6e512ca52f97371f07f01ac2c483fe8eef8
vzhomeexperiments <vz.home.experiments@gmail.com>
2021-02-28 09:46:10 +0100

    update with Env.Variables

--- _PushToMT4Prod.bat
+++ _PushToMT4Prod.bat
@@ -5,10 +5,10 @@ rem Use when you need to publish all files to All Terminals
 setlocal enabledelayedexpansion
 
 set SOURCE_DIR="%PATH_DSS_Repo%\Include"
-set DEST_DIR1=%PATH_T1_I%
-set DEST_DIR2=%PATH_T2_I%
-set DEST_DIR3=%PATH_T3_I%
-set DEST_DIR4=%PATH_T4_I%
+set DEST_DIR1="%PATH_T1_I%"
+set DEST_DIR2="%PATH_T2_I%"
+set DEST_DIR3="%PATH_T3_I%"
+set DEST_DIR4="%PATH_T4_I%"
 
 ROBOCOPY %SOURCE_DIR% %DEST_DIR1% *.mqh
 ROBOCOPY %SOURCE_DIR% %DEST_DIR2% *.mqh
